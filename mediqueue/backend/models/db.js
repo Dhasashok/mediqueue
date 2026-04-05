263a4cdc8b86fdc0272c00c968cf670f99ff753f
Dhasashok <146851861+Dhasashok@users.noreply.github.com>
2026-04-06 00:22:01 +0530

    New Chnages

--- mediqueue/backend/models/db.js
+++ mediqueue/backend/models/db.js
@@ -1,15 +1,17 @@
 const mysql = require('mysql2');
-
+ 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'mediqueue',
+  host:     process.env.DB_HOST     || 'kx1s-e.h.filess.io',
+  port:     process.env.DB_PORT     || 3307,
+  user:     process.env.DB_USER     || 'mediqueue_worejoined',
+  password: process.env.DB_PASSWORD || '0f7d313db08394d30d7c8b2fce657ecdc50e93a6',
+  database: process.env.DB_NAME     || 'mediqueue_worejoined',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
-
+ 
 const db = pool.promise();
-
+ 
 module.exports = db;
+ 
